50a8c7cd57c67ef00c04e41e5af403d6981a22e7
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-02-19 03:57:13 +0600

    scroll functionality added

---
+++ functionality.js
@@ -0,0 +1,5 @@
+//scroll to ticket section
+function scrollToTicketSection(){
+    var ticketSection = document.getElementById('target-section');
+    ticketSection.scrollIntoView({behavior: "smooth"});
+}
